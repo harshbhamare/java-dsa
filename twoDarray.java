502194106404e3d2d7a787a0c1dd13084bb85cc2
Harsh Bhamare <harshbhamaremit@gmail.com>
2026-02-25 19:31:47 +0530

    two d araay start

---
+++ twoDarray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class twoDarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows : ");
+        int row = sc.nextInt();
+        System.out.println("Enter number of columns : ");
+        int column = sc.nextInt();
+
+        int[][] numbers = new int[row][column];
+
+
+        // input
+        for(int i=0 ; i<row ; i++){
+            for(int j=0 ; j<column ; j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        // printing 2d array
+        for(int i=0 ; i<row ; i++){
+            for(int j=0 ; j<column ; j++){
+                System.out.print(numbers[i][j] + "    ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+
+    }
+}
